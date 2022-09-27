9f12684a1dc0723a7f6a97d0a1c88878b9a8a29f
Nina Verschuuren <Nina@ww120267.uvt.nl>
2022-09-27 14:21:21 +0200

    Dummies from host chararcteristics

--- src/data-preparation/clean_data.R
+++ src/data-preparation/clean_data.R
@@ -1,11 +1,15 @@
-# Load merged data 
-load("./gen/data-preparation/temp/data_merged.RData")
+# Make dummy of the variable "host_is_superhost"
+low_inflation_dataset$host_is_superhost <- ifelse(low_inflation_dataset$host_is_superhost == "True", 1, 0)
+high_inflation_dataset$host_is_superhost <- ifelse(high_inflation_dataset$host_is_superhost == "True", 1, 0)
+full_dataset_cities$host_is_superhost <- ifelse(full_dataset_cities$host_is_superhost == "True", 1, 0)
 
-# Drop observations with V1 <= -0.9
-df_cleaned <- df_merged[df_merged$V1 > -0.9,]
+# Make dummy of the variable "host_has_profile_pic" 
+low_inflation_dataset$host_has_profile_pic <- ifelse(low_inflation_dataset$host_has_profile_pic == "TRUE", 1, 0)
+high_inflation_dataset$host_has_profile_pic <- ifelse(high_inflation_dataset$host_has_profile_pic == "TRUE", 1, 0)
+full_dataset_cities$host_has_profile_pic <- ifelse(full_dataset_cities$host_has_profile_pic == "TRUE", 1, 0)
 
-# Remove V1
-df_cleaned <- df_cleaned[,c(1,2,4:7)]
+# Make dummy of the variable "host_identity_verified"
+low_inflation_dataset$host_identity_verified <- ifelse(low_inflation_dataset$host_is_superhost == "True", 1, 0)
+high_inflation_dataset$host_identity_verified <- ifelse(high_inflation_dataset$host_is_superhost == "True", 1, 0)
+full_dataset_cities$host_identity_verified <- ifelse(full_dataset_cities$host_is_superhost == "True", 1, 0)
 
-# Save cleaned data
-save(df_cleaned,file="./gen/data-preparation/output/data_cleaned.RData")
