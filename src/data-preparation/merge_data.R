3bc474eee7532aebf79b1b25ff260f4a4f3ca5d2
Nynke Voermans <nynkevoermans@ww020926.uvt.nl>
2022-10-03 14:06:05 +0200

    made every name in lower capital and made inflation a numeric variable

--- src/data-preparation/merge_data.R
+++ src/data-preparation/merge_data.R
@@ -15,29 +15,29 @@ Athens <- read.csv("greeceatticaathens.csv")
 # Make new variable with city for each city
 library(dplyr)
 # High inflation
-Capetown$city_name <- c("Capetown")
-Santiago$city_name <- c("Santiago")
-Mexico$city_name <- c("Mexico")
-Rio$city_name <- c("Rio")
-Boston$city_name <- c("Boston")
+Capetown$city_name <- c("capetown")
+Santiago$city_name <- c("santiago")
+Mexico$city_name <- c("mexico")
+Rio$city_name <- c("rio")
+Boston$city_name <- c("boston")
 # Low inflation
-Tokyo$city_name <- c("Tokyo")
-Geneva$city_name <- c("Geneva")
-Beijing$city_name <- c("Beijing")
-Bangkok$city_name <- c("Bankok")
-Athens$city_name <- c("Athens")
+Tokyo$city_name <- c("tokyo")
+Geneva$city_name <- c("geneva")
+Beijing$city_name <- c("beijing")
+Bangkok$city_name <- c("bankok")
+Athens$city_name <- c("athens")
 
 # Make dataset for high inflation cities
 high_inflation_dataset <- bind_rows(Capetown, Santiago, Mexico, Rio, Boston)
 
 # Make dummy variable for high inflation cities 
-high_inflation_dataset$inflation <- c("0")
+high_inflation_dataset$inflation <- 0
 
 # Make dataset for low inflation cities
 low_inflation_dataset <- bind_rows(Tokyo, Geneva, Beijing, Bangkok, Athens)
 
 # Make dummy variable for low inflation cities 
-low_inflation_dataset$inflation <- c("1")
+low_inflation_dataset$inflation <- 1
 
 # Combine both datasets in full dataset
 full_dataset_cities <- bind_rows(low_inflation_dataset, high_inflation_dataset)
