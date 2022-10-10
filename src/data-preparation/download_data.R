b2adfba84f0375027524498653f9f9271ea78f6b
amber2105 <62336902+amber2105@users.noreply.github.com>
2022-10-10 12:54:32 +0200

    Changed the source code to download the data

--- src/data-preparation/download_data.R
+++ src/data-preparation/download_data.R
@@ -1,22 +1,30 @@
-# Download datasets
+dir.create('../../data')
 
-urls = c('http://data.insideairbnb.com/united-states/ma/boston/2022-06-13/data/listings.csv.gz',
-         'http://data.insideairbnb.com/brazil/rj/rio-de-janeiro/2022-06-20/data/listings.csv.gz',
-         'http://data.insideairbnb.com/mexico/df/mexico-city/2022-06-21/data/listings.csv.gz',
-         'http://data.insideairbnb.com/south-africa/wc/cape-town/2022-06-22/data/listings.csv.gz',
-         'http://data.insideairbnb.com/chile/rm/santiago/2022-06-22/data/listings.csv.gz',
-         'http://data.insideairbnb.com/japan/kant%C5%8D/tokyo/2022-06-24/data/listings.csv.gz',
-         'http://data.insideairbnb.com/switzerland/geneva/geneva/2022-06-22/data/listings.csv.gz',
-         'http://data.insideairbnb.com/china/beijing/beijing/2022-06-21/data/listings.csv.gz',
-         'http://data.insideairbnb.com/thailand/central-thailand/bangkok/2022-06-20/data/listings.csv.gz',
-         'http://data.insideairbnb.com/greece/attica/athens/2022-06-17/data/listings.csv.gz')
+# creating a list with the download link and filenames of the raw data 
+files = list(c(url='http://data.insideairbnb.com/united-states/ma/boston/2022-06-13/data/listings.csv.gz',
+               fn='listings-united-states.csv.gz'),
+             c(url='http://data.insideairbnb.com/brazil/rj/rio-de-janeiro/2022-06-20/data/listings.csv.gz',
+               fn='listings-brazil.csv.gz'),
+             c(url='http://data.insideairbnb.com/mexico/df/mexico-city/2022-06-21/data/listings.csv.gz',
+               fn='listings-mexico.csv.gz'),
+             c(url='http://data.insideairbnb.com/south-africa/wc/cape-town/2022-06-22/data/listings.csv.gz',
+               fn='listings-south-africa.csv.gz'),
+             c(url='http://data.insideairbnb.com/chile/rm/santiago/2022-06-22/data/listings.csv.gz',
+               fn='listings-chile.csv.gz'),
+             c(url='http://data.insideairbnb.com/japan/kant%C5%8D/tokyo/2022-06-24/data/listings.csv.gz',
+               fn='listings-japan.csv.gz'),
+             c(url='http://data.insideairbnb.com/switzerland/geneva/geneva/2022-06-22/data/listings.csv.gz',
+               fn='listings-switzerland.csv.gz'),
+             c(url='http://data.insideairbnb.com/china/beijing/beijing/2022-06-21/data/listings.csv.gz',
+               fn='listings-china.csv.gz'),
+             c(url='http://data.insideairbnb.com/thailand/central-thailand/bangkok/2022-06-20/data/listings.csv.gz',
+               fn='listings-thailand.csv.gz'),
+             c(url='http://data.insideairbnb.com/greece/attica/athens/2022-06-17/data/listings.csv.gz',
+               fn='listings-greece.csv.gz')
+)
 
-for (url in urls) {
-  filename = paste0(gsub('[^a-z]', '', url), '.csv') 
-  filename = gsub('httpdatainsideairbnbcom', '', filename)
-  filename = gsub('datalistingscsvgz', '', filename)
-  download.file(url, destfile = filename) 
+# looping over the list to download and save the file in the data folder
+for (item in files) {
+  
+  download.file(item['url'], paste0('../../data/', item['fn']))
 }
-
-
-
